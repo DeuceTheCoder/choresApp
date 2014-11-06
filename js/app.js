869f4036b136edca6d99bd05eb46f677f88ada59
Coleman Beasley <cbeasley@thoughtworks.com>
2014-11-06 14:57:38 -0800

    initial commit. Lists chores and residents. Issue when chore is not listed...

---
+++ js/app.js
@@ -0,0 +1,58 @@
+App = Ember.Application.create();
+
+App.Router.map(function() {
+  this.route('chores');
+  this.resource('chore', {path: '/chores/:name'});
+  this.route('residents');
+  this.resource('resident', {path: '/residents/:name'});
+});
+
+App.IndexRoute = Ember.Route.extend({
+  model: function() {
+    return ['red', 'yellow', 'blue'];
+  }
+});
+App.ChoresRoute = Ember.Route.extend({
+  model: function() {
+    return App.CHORES;
+  }
+});
+App.ChoreRoute = Ember.Route.extend({
+    model: function(params) {
+        return App.CHORES.findBy('name', params.name);
+    }
+});
+App.ResidentsRoute = Ember.Route.extend({
+  model: function() {
+    return App.RESIDENTS;
+  }
+});
+App.ResidentRoute = Ember.Route.extend({
+    model: function(params) {
+        return App.RESIDENTS.findBy('name', params.name);
+    }
+});
+
+App.CHORES = [
+  {
+    name: 'Sweeping',
+    frequency: 'daily',
+    description: 'Sweep the kitchen and dining room floors',
+    firstChoosers: []
+  },
+  {
+    name: 'Mopping',
+    frequency: 'weekly',
+    description: 'Mop the kitchen and dining room floors',
+    firstChoosers: []
+  }
+];
+
+App.RESIDENTS = [
+  {
+    name:'Robert',
+    avatarImage: '',
+    firstChoices: ['Sweeping', 'Mopping', 'Dishes'],
+    assignedChores: []
+  }
+];
